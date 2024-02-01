77625786a734e2822a7ccf5fc30080141f9b28b2
Kristian Karlson <83krikar@gafe.molndal.se>
2024-02-01 10:53:44 +0100

    inti comit

---
+++ src/main/java/com/karlson/fruitBax/FruitBaxApplication.java
@@ -0,0 +1,13 @@
+package com.karlson.fruitBax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitBaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitBaxApplication.class, args);
+	}
+
+}
