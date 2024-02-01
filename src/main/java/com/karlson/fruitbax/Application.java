e8a4597d1017fbd1e5d711b504d954f733f8fddb
Kristian Karlson <83krikar@gafe.molndal.se>
2024-02-01 12:03:16 +0100

    add http client to meet package goal

--- src/main/java/com/karlson/fruitbax/Application.java
+++ src/main/java/com/karlson/fruitbax/Application.java
@@ -1,13 +1,22 @@
 package com.karlson.fruitbax;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner() { // todo remove or add content
+        return args -> {
+            System.out.println("Hello World!");
+        };
+    }
 
 }
